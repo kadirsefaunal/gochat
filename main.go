1a149f21971610e727fb5a89ae5d65abdff725b8
kadirsefaunal <kadirsefau@gmail.com>
2018-12-11 17:48:02 +0300

    Project created.

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/googollee/go-socket.io"
+)
+
+type Message struct {
+	From    string `json:"from"`
+	To      string `json:"to"`
+	Message string `json:"message"`
+}
+
+type User struct {
+	UserName string `json:"username"`
+	Message  string `json:"message"`
+}
+
+func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users := make(map[string]socketio.Socket)
+
+	server.On("connection", func(so socketio.Socket) {
+		so.On("set-user", func(user string) {
+			users[user] = so
+			fmt.Println(users)
+		})
+
+		so.On("message", func(msg string) {
+			jsonMsg := Message{}
+			json.Unmarshal([]byte(msg), &jsonMsg)
+			fmt.Println(jsonMsg)
+
+			toSo := users[jsonMsg.To]
+			if toSo != nil {
+				toSo.Emit("message", jsonMsg)
+			}
+		})
+
+		so.On("disconnection", func() {
+			var k string
+			for key, socket := range users {
+				if socket == so {
+					k = key
+					break
+				}
+			}
+
+			delete(users, k)
+
+			user := User{
+				UserName: k,
+				Message:  "User left!",
+			}
+
+			for _, socket := range users {
+				socket.Emit("message", user)
+			}
+
+			fmt.Println(users)
+			log.Println("Disconnect!")
+		})
+	})
+
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:5000 ...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
